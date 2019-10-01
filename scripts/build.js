09b4b509eba424562caf645a8c24911d9587c1d4
Arjun Mahishi <arjun.mahishi@gmail.com>
2019-10-01 12:27:04 +0530

    move build.js to scripts directory

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,25 @@
-var browserify = require('browserify');
-var babelify = require('babelify');
+const fs = require('fs-extra');
+const browserify = require('browserify');
 
-b = browserify().transform(babelify.configure({
-    presets: ["es2015"]
-}))
+const buildDirectory = 'build';
+const filesToBundle = [
+    ['src/content.js', 'content-script.js'],
+    ['src/background.js', 'background-script.js'],
+];
 
+const directoriesToCopy = ['src/popup/', 'public']
+
+const bundle = ([fileIn, fileOut]) => {
+    browserify().add(fileIn).bundle().pipe(fs.createWriteStream(`${buildDirectory}/${fileOut}`))
+}
+
+const copy = (directory) => fs.copy(directory, buildDirectory)
+
+const build = async () => {
+    await fs.remove(buildDirectory)
+    await fs.ensureDir(buildDirectory)
+    filesToBundle.map(bundle);
+    directoriesToCopy.map(copy)
+}
+
+build();
