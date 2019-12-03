52404f48daae0770332b09eee7914d9efb6ed510
Vicky-Raj <varthanv26018@gmail.com>
2019-12-03 21:23:11 +0530

    Solved the issue #12

--- src/content.js
+++ src/content.js
@@ -1,5 +1,19 @@
-var parsers = require('./lib/content/parser')
-
-var parser = parsers.NewParser()
-
-console.log(parser.getTitle(), parser.getLink())
+window.onload = () => {
+  var title = document.querySelector("title").text;
+  console.log(title.text);
+  const metas = document.getElementsByTagName("meta");
+  var data = "";
+  for (let i = 0; i < metas.length; i++) {
+    if (
+      metas[i].getAttribute("name") === "description" ||
+      metas[i].getAttribute("name") === "summary"
+    ) {
+      console.log(metas[i].getAttribute("content"));
+      data += metas[i].getAttribute("content");
+    }
+  }
+  chrome.runtime.sendMessage({
+    action: "getTitle",
+    source: `<h1> ${title} - ${data} </h1>`
+  });
+};
