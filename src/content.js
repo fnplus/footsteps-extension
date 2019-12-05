fddd6d3c7cec12894fe07987d5b22290d9d3edd2
Vicky-Raj <varthanv26018@gmail.com>
2019-12-05 16:05:02 +0530

    Variable Changes and valid Debug messages

--- src/content.js
+++ src/content.js
@@ -1,19 +1,21 @@
 window.onload = () => {
   var title=document.querySelector('title').text;
   const metas = document.getElementsByTagName("meta");
-  var data = "";
+  var metaData = "";
   for (let i = 0; i < metas.length; i++) {
     if (
       metas[i].getAttribute("name") === "description" ||
       metas[i].getAttribute("name") === "summary"
     ) {
       
-      data += metas[i].getAttribute("content");
+      metaData += metas[i].getAttribute("content");
     }
-    // Debug message
-    console.log(title +'' + data);
+
 
   }
+  // Debug message to make sure it Works
+  console.log(title+ metaData);
+
   chrome.runtime.sendMessage({
     action: "getTitle",
     source: {
